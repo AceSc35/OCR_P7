3355bfdfebfece50ccfc92767dfab6d4da413172
AceSc35 <baptiste.lef@hotmail.fr>
2022-04-11 17:10:40 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+
+const app = express();
+
+//Cors
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+//Req JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: './config/.env' });
+const { sequelize } = require('./models');
+
+const http = require('http');
+const app = require('./app');
+
+app.set('port', process.env.PORT || 5000);
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 5000, async () => {
+  console.log(`Listening on port ${process.env.PORT}`.yellow);
+  await sequelize.sync({});
+  console.log('Connexion à la base de donnée : Ok');
+});
