9c8a836d834f451f891d31ff477046084bf9ef22
AceSc35 <baptiste.lef@hotmail.fr>
2022-04-26 18:17:00 +0200

    start frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Validax</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
